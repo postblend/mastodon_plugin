4018f3f29005efe5cc0ea703a42adc1af59e3ac2
Paul Brown <paul.brown@kde.org>
2022-05-22 23:22:56 +0200

    adding instance and its secrets to db

--- mastodon_plugin.py
+++ mastodon_plugin.py
@@ -1,7 +1,8 @@
-# SPDX-FileCopyrightText: 2022 Claudio Cambra <claudio.cambra@gmail.com>
+# SPDX-FileCopyrightText: 2022 Paul Brown <pbrown@mykolab.com>
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
 from dataclasses import dataclass
+from mastodon import Mastodon
 
 import core.definitions
 from core.coredatabase import CoreDatabase
@@ -14,14 +15,12 @@ import core.api.v1.database as database
 class MastodonAccount(BasicPlatformAccount):
     """
     BasicPlatformAccount provides 4 generic fields
-
-        * id = identification number of th account (calculated automatically)
-        * name = name of the account, i.e. something the user can remembr the account
+    * id = identification number of th account (calculated automatically)
+    * name = name of the account, i.e. something the user can remembr the account
     by (e.g. "Work Account", "Jokes & Politics", etc.)
-        * username = user name used to log in to the account
-        * password = the password used to log in to the account
-
-    (In that order)
+    * username = user name used to log in to the account
+    * password = the password used to log in to the account
+    (in that order)
 
     To add extra fields (and their type) you need to inherit
     BasicPlatformAccount into a new class and then add your own variables.
@@ -34,17 +33,24 @@ class MastodonPlugin(PlatformPluginBase):
     def __init__(self):
         self.id = "mastodon_plugin"
         self.name = "Mastodon Plugin"
-        self.author = "Meh"
-        self.author_url = ""
+        self.author = "Paul Brown"
+        self.author_url = "https://quickfix.es"
         self.description = "A plugin to post to Mastodon :)"
 
+        # Mastodon user table fields
         self.name_field = "name"
         self.username_field = "username"
         self.password_field = "password"
         self.instance_field = "instance"
 
+        # Mastodon instance table fields
+        self.url_field = "url" # URL WUTHOUT "https://"
+        self.client_id_field = "client_id"
+        self.client_secret_field = "client_secret"
+
 
     def init_database_data(self):
+        # Get user table ready or create it if it doesn't exist
         self.plugin_table_name = "mastodon_users_table"
         self.table_data_fields = (
             database.DatabaseFieldDefinition ((self.name_field, "TEXT", "")),
@@ -55,17 +61,15 @@ class MastodonPlugin(PlatformPluginBase):
 
         database.create_plugin_table(self.plugin_table_name, self.table_data_fields)
 
-        # Add user to table
-        #   Fields: user, password, instance
-        # Check to see if there is another user with the same instance
-        # if not register app with the instance and download secrets to file
-        # create a new entry for the instance in the instance table
-        #   Fields: instance, secret 1, secret 2 <- Read from File
-        # remove the file
+        # Get mastodon instance table ready or create it if it doesn't exist
+        self.instance_table_name = "mastodon_instances_table"
+        self.table_instance_data_fields = (
+            database.DatabaseFieldDefinition ((self.url_field, "TEXT", "")),
+            database.DatabaseFieldDefinition ((self.client_id_field, "TEXT", "")),
+            database.DatabaseFieldDefinition ((self.client_secret_field, "TEXT", ""))
+        )
 
-        pass
-
-
+        database.create_plugin_table(self.instance_table_name, self.table_instance_data_fields)
 
     def cleanup_database_data(self):
         account_ids = self.account_ids()
@@ -118,6 +122,12 @@ class MastodonPlugin(PlatformPluginBase):
 
 
     def add_account(self, account_details: dict) -> int:
+        # Check to see if the user's instance is already registered and, if not,
+        # add it to the instance table
+
+        if not database.plugin_data_value(self.instance_table_name, self.url_field, account_details[self.instance_field]):
+            self.add_instance(account_details[self.instance_field])
+
         return database.add_plugin_data(self.plugin_table_name, account_details)
 
 
@@ -127,6 +137,21 @@ class MastodonPlugin(PlatformPluginBase):
 
     def delete_account(self, account_id: int):
         database.delete_plugin_data(self.plugin_table_name, account_id)
+
+    def add_instance(self, instance):
+        # Grab secrets from mastodon instance
+        secrets = (Mastodon.create_app(client_name = "PostBlendTest", api_base_url = instance))
+
+        # Create a new entry for the instance in the instance table
+        instance_details = {
+                self.url_field : instance,
+                self.client_id_field : secrets[0],
+                self.client_secret_field : secrets[1]
+                }
+
+        database.add_plugin_data(self.instance_table_name, instance_details)
+
+
 
 
     def publish_post(self, post: PostBase, account_ids: tuple):
