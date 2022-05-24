5618cfe38bf533bb40bb60c0c788352570b3072c
Paul Brown <paul.brown@kde.org>
2022-05-25 01:30:31 +0200

    Removed old test file

--- run_mastodon.py
+++ run_mastodon.py
@@ -19,20 +19,22 @@ plugin_manager._available_plugins.append (mastodon_plugin)
 
 # Add user
 
-"""
+
 mastodon_user = {
-        mastodon_plugin.name_field: "Work",
+        mastodon_plugin.name_field: "Bingo Bango",
         mastodon_plugin.username_field: "some@email.com",
         mastodon_plugin.password_field: "somepassword",
-        mastodon_plugin.instance_field: "some.mastodon.instance",
+        mastodon_plugin.instance_field: "mastodon.social",
         }
 
 row_id = mastodon_plugin.add_account (mastodon_user)
-"""
 
+
+# Post to accounts 1 and 4
+"""
 mastodon_test_post = PostBase
 mastodon_test_post.title = "TITLE"
 mastodon_test_post.body = "PB test post"
 
-# Post to accounts 1 and 4
 plugin_manager.publish_post(mastodon_test_post, {"mastodon_plugin": (1,4)})
+"""
